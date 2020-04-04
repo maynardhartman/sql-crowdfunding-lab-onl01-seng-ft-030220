5da163723def45763ff11d6e5a53c3df4d4539a9
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-04 00:17:46 +0000

    Automatically backed up by Learn

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source "https://rubygems.org"
 
 # Database
 gem 'sqlite3', '~>1.3.6'
-
+# gem 'mysql2'
 # Testing
 gem 'rspec'
---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER);
+  
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  title     TEXT,
+  category  TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
+  end_date   DATE);
+  
+CREATE TABLE pledges (
+  id  INTEGER PRIMARY KEY,
+  amount  MONEY,
+  user_id INTEGER,
+  project_id INTEGER,
+  FOREIGN KEY(user_id, project_id));
