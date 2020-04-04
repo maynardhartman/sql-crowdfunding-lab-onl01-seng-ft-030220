2c0302ae64f0430b9c327a9730dc6c88ccb64157
Maynard Hartman <mjhartman@brighthouse.com>
2020-04-04 00:31:58 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,10 +5,7 @@ CREATE TABLE 'projects' (
   
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
-  category TEXT,
-  funding_goal INTEGER,
-  start_date DATE,
-  end_date DATE);
+  
   name TEXT,
   age INTEGER);
 CREATE TABLE pledges (
